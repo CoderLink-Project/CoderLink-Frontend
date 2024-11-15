77b33d11a45b95aa37972497adf164406684ab46
henryflores04 <wfloresj1@upao.edu.pe>
2024-11-15 00:23:29 -0500

    Add files via upload

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,22 +1,97 @@
-import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment.development';
-import { Observable } from 'rxjs';
+import { Inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { MainService } from './main.service';
 import { HttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
+import { environment } from '../../environments/environment.development';
+import { DOCUMENT } from '@angular/common';
+import { User } from '../_models/datos-user';
+import { Customer } from '../_models/datos-customer';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
-  private apiUrl = `${environment.apiUrl}/auth`;
+export class AuthService extends MainService{
+  HttpClient: any
+  
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private sanitizer: DomSanitizer,
+    private router: Router,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    super(http, sanitizer);
+  }
 
-  changePassword(currentPassword: string, newPassword: string): Observable<any> {
-    const payload = { currentPassword, newPassword };
-    return this.http.post(`${this.apiUrl}/change-password`, payload);
+  localStorage = this.document.defaultView?.localStorage;
+  flagShowLogin: boolean = false;
+
+  async loginService(data: any){
+    debugger
+    
+    const user = {
+      "email": data.email,
+      "password": data.password
+    }
+    const response = await this.postAsyncPass(environment.BASE_URL+'/auth/login', user)
+    if(response.token){
+      
+      localStorage.setItem("userRol", response.role)
+      localStorage.setItem("nombreCompletoUser", response.firstName+' '+response.lastName)
+      this.localStorage?.setItem("userToken", response.token);
+
+      if(response.role == "CUSTOMER"){
+        await this.router.navigate(['/contacto']);
+      }else if(response.role == "DEVELOPER"){
+        await this.router.navigate(['/perfil-developer']);
+      }else if (response.role == "ADMIN"){
+        await this.router.navigate(['/contacto']);
+      }
+
+      
+    }
+    
+
   }
-  isAuthenticated(): boolean {
-    // Ejemplo de lógica de autenticación (puedes cambiar esto según tus necesidades)
-    return !!localStorage.getItem('authToken');
+
+  isLogged():boolean{
+    
+    return false;
+    
   }
+  
+  async loginUser(user: any) {
+  
+  localStorage.setItem('user_name', user.name);
+  localStorage.setItem('user_email', user.email)
+  localStorage.setItem('user_token', user.token);
+
+  await this.router.navigate(['/admin']);
+  }
+
+  async logout(){
+    localStorage.clear();
+    await this.router.navigate(['/login']);
+  }
+
+  async createDeveloper(usuario : User) : Promise<boolean>{
+    const response = await this.postAsync(environment.BASE_URL+'/auth/register/developer', usuario)
+    if(response){
+      this.flagShowLogin = true;
+    }
+    
+    return this.flagShowLogin;
+  }
+
+  async createCustomer(usuario : User) : Promise<boolean>{
+    const response = await this.postAsync(environment.BASE_URL+'/auth/register/customer', usuario)
+    
+    if(response){
+      this.flagShowLogin = true;
+    }
+    return this.flagShowLogin
+
+  }
+
 }
