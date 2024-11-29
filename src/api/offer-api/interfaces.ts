373d9031bd1d14d198d6b6ccc0e082f88ea23413
jmlc643 <166523842+raul151999@users.noreply.github.com>
2024-11-29 15:57:39 -0500

    Update interfaces.ts

--- src/api/offer-api/interfaces.ts
+++ src/api/offer-api/interfaces.ts
@@ -1,14 +1,1 @@
-import { Postulation } from "../postulation-api/interfaces"
 
-export interface JobOffer{
-    id: number
-    budget : number,
-    publicationDate : Date,
-    postulation : Postulation
-}
-
-export interface CreateJobOfferRequest{
-    budget : number,
-    customerUsername : string,
-    postulationId : number
-}
