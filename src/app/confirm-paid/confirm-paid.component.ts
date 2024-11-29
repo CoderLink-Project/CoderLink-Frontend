7efd71d92f2cf2f0fb4d024cd90bdb6986361bdb
jmlc643 <166523842+raul151999@users.noreply.github.com>
2024-11-29 15:22:54 -0500

    Funcionalidad para capturar pedido y redirigir al perfil del cliente

--- src/app/confirm-paid/confirm-paid.component.ts
+++ src/app/confirm-paid/confirm-paid.component.ts
@@ -16,20 +16,20 @@ export class ConfirmPaidComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
   paymentApiService = inject(PaymentApiService)
   router = inject(Router)
-
+  
+onTransfer(): void {
+    const orderId: CaptureOrderRequest = {
+      orderId: this.orderId as string
+    } 
+    this.paymentApiService.captureOrder(orderId)
+    this.router.navigateByUrl('/profile-customer')
+  }
+  
   ngOnInit(): void {
     // Obtener el parámetro "orderId" desde la URL o de algún otro método
     this.route.queryParams.subscribe(params => {
       this.orderId = params['token'] || null;
       console.log('Order ID:', this.orderId);
     });
-  }
-
-  onTransfer(): void {
-    const orderId: CaptureOrderRequest = {
-      orderId: this.orderId as string
-    } 
-    this.paymentApiService.captureOrder(orderId)
-    this.router.navigateByUrl('/profile-customer')
   }
 }
